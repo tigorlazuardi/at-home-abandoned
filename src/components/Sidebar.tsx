a1cb8defe44a75e30addcbf53e8506f6eab57268
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-19 09:19:25 +0700

    learn: implemented sidebar discord like

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import { BsFillLightningFill, BsGearFill, BsPlus } from 'react-icons/bs'
+import { FaFire, FaWrench } from 'react-icons/fa'
+
+interface SideBarProp {
+	icon_size?: number
+}
+
+const SideBar = (prop: SideBarProp) => {
+	const icon_size = prop.icon_size ?? 28
+	return (
+		<div className='fixed top-0 left-0 flex flex-col w-16 h-screen m-0 text-white shadow-lg bg-discord-800'>
+			<SideBarIcon text='Fire' icon={<FaFire size={icon_size} />} />
+			<SideBarIcon text='Wrench' icon={<FaWrench size={icon_size} />} />
+			<SideBarIcon text='Plus' icon={<BsPlus size={icon_size} />} />
+			<SideBarIcon text='Lightning' icon={<BsFillLightningFill size={icon_size} />} />
+			<SideBarIcon text='Gear' icon={<BsGearFill size={icon_size} />} />
+		</div>
+	)
+}
+
+interface SideBarIconProp {
+	icon: React.ReactElement
+	text?: string
+}
+
+const SideBarIcon = ({ icon, text }: SideBarIconProp) => {
+	return (
+		<div
+			className={`flex relative justify-center items-center
+					mx-auto w-12 h-12 text-green-500 rounded-3xl
+					transition-all duration-200 bg-discord-700 hover:bg-green-600
+					hover:text-white hover:rounded-xl ease-liner group`}
+		>
+			{icon}
+			<Tooltip text={text}></Tooltip>
+		</div>
+	)
+}
+
+interface TooltipProp {
+	text?: string
+}
+
+const Tooltip = ({ text }: TooltipProp) =>
+	text ? (
+		<span
+			className={`absolute w-auto p-2 m-2 min-w-max left-14
+						rounded-md shadow-md text-white bg-discord-900
+						text-xs lg:text-lg fon-bold transition-all duration-100
+						scale-0 origin-right group-hover:scale-100 sm-max:hidden`}
+		>
+			{text}
+		</span>
+	) : (
+		<></>
+	)
+
+export default SideBar
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,27 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+		},
 		extend: {
+			screens: {
+				'2xl-max': { max: '1535px' },
+				// => @media (max-width: 1535px) { ... }
+
+				'xl-max': { max: '1279px' },
+				// => @media (max-width: 1279px) { ... }
+
+				'lg-max': { max: '1023px' },
+				// => @media (max-width: 1023px) { ... }
+
+				'md-max': { max: '767px' },
+				// => @media (max-width: 767px) { ... }
+
+				'sm-max': { max: '639px' },
+				// => @media (max-width: 639px) { ... }
+			},
 			fontFamily: {
 				primary: ['Inter', ...fontFamily.sans],
 			},
@@ -34,13 +54,24 @@ module.exports = {
 					800: withOpacity('--tw-clr-primary-800'),
 					900: withOpacity('--tw-clr-primary-900'),
 				},
+				discord: {
+					900: '#202225',
+					800: '#2f3136',
+					700: '#36393f',
+					600: '#4f545c',
+					400: '#d4d7dc',
+					300: '#e3e5e8',
+					200: '#ebedef',
+					100: '#f2f3f5',
+				},
 				dark: '#222222',
 			},
 			keyframes: {
 				flicker: {
 					'0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
 						opacity: 0.99,
-						filter: 'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
+						filter:
+							'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
 					},
 					'20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
 						opacity: 0.4,
